02ac50b9688d6fe8be37c32d69b1eea8407f9c01
1234 <arpitsinghthakur321@gmail.com>
2024-01-02 23:15:21 +0530

    Footer finished and changed title logo

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,10 +1,92 @@
-import React from 'react'
-import "./Footer.scss";
+import React, { useState } from 'react';
+import { BsLinkedin } from "react-icons/bs";
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
+import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default Footer
+  const { username, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card ">
+          <img src={images.email} alt="email" />
+          <a href="mailto:web3arpit@gmail.com" className="p-text">web3arpit@gmail.com</a>
+        </div>
+        <div className="app__footer-card">
+        <BsLinkedin style={{ fontSize: '36px', marginRight: '8px' }} />
+          {/* <img src={images.mobile} alt="phone" />
+          <a href="tel:+91 " className="p-text">+91 </a> */}
+ 
+          <a href="https://www.linkedin.com/in/arpitweb3" style={{ fontSize: '14px', marginLeft: '8px' }} className="p-text"> arpitweb3 </a>
+        </div>
+      </div>
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank you for getting in touch!
+            <br />
+            {/* Would love to Connect on Linkedin as well */}
+          </h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg',
+);
